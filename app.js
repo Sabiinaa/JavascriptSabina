67b45e88befdf9cc6058545b01b0bfedf80489a0
Sabiinaa <sebinemurquzova1+sabii@mail.ru>
2022-07-14 22:03:49 +0000

    demo

--- app.js
+++ app.js
@@ -1,41 +1,20 @@
 "use strict";
 $(document).ready(function () {
 
-});
-
-
-
-// $('h1').click(function (e) {
-//     e.preventDefault();
-//     $('input').html(1234);
-// })
-
-
-// $('.btn').click(function (e) {
-//     e.preventDefault();
-
-//     let a = (a) => {
-//         for (let i = 1; i <= a; i++) {
-//             $('.contentDiv').append(`h1>{a}</h1`);
-//         }
-//     } else{
-//         $('.contentDiv').append(`<h1>${a}</h1`)
-
-//     }
-//     input.value = "";
-
-// });
-$('.btn').click(function (e) {
-    e.preventDefault();
-    let func = a => {
-        if (Number(a)) {
-            for (let i = 1; i <= a; i++) {
+    $('.btn').click(function (e) {
+        e.preventDefault();
+        let func = a => {
+            if (Number(a)) {
+                for (let i = 1; i <= a; i++) {
+                    $('.smallbox').append(`<h1>${a}</h1>`);
+                }
+            } else {
                 $('.smallbox').append(`<h1>${a}</h1>`);
             }
-        } else {
-            $('.smallbox').append(`<h1>${a}</h1>`);
+            input.value = "";
         }
-        input.value = "";
-    }
-    func($('input').val());
+        func($('input').val());
+    });
 });
+
+
